ff37a599667f3cf376a37d74b845119617df7727
Kurva2076 <85439926+Kurva2076@users.noreply.github.com>
2023-10-25 18:38:39 +0300

    version 2

--- 7/main.js
+++ 7/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  $(".slider").slick();
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 })

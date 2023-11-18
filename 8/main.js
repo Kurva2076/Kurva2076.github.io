0fb3d242bed3dfb1aa92caa4c57dc1bca52e1f52
Kurva2076 <85439926+Kurva2076@users.noreply.github.com>
2023-11-19 00:32:57 +0300

    version 6

--- 8/main.js
+++ 8/main.js
@@ -1,5 +1,8 @@
 const win = window;
 const doc = document;
+const his = history;
+const con = console;
+const Slap = new Slapform();
 const serMsg = doc.getElementsByClassName("service-message")[0];
 const popup = doc.getElementsByClassName("popup")[0];
 const infForUsr = doc.getElementsByClassName("infoForUser")[0];
@@ -8,18 +11,18 @@ win.addEventListener("DOMContentLoaded", function () {
     const popupOpenButton = doc.getElementsByClassName("openPopup")[0];
     const popupCloseButton = doc.getElementsByClassName("closePopup")[0];
     const sendInfoButton = doc.getElementsByClassName("sendButton")[0];
-    popupOpenButton.addEventListener("click", function() {
+    popupOpenButton.addEventListener("click", function () {
         openPopup();
     });
-    addEventListener("popstate", function() {
+    win.addEventListener("popstate", function () {
         closePopup();
     });
-    popupCloseButton.addEventListener("click", function() {
+    popupCloseButton.addEventListener("click", function () {
         closePopup();
     });
-    sendInfoButton.addEventListener("click", function(event) {
-        event.preventDefault();
+    sendInfoButton.addEventListener("click", function (event) {
         var resultOfCheck = checkForm();
+        event.preventDefault();
         if (resultOfCheck) {
             serMsg.style.display = "none";
             submitForm();
@@ -31,26 +34,27 @@ win.addEventListener("DOMContentLoaded", function () {
 });
 
 function openPopup() {
-    history.pushState(null, null, '#popupForm');
+    his.pushState(null, null, "#popupForm");
     popup.style.display = "block";
     insertLastInformation();
 }
 
 function closePopup() {
     popup.style.display = "none";
-    history.pushState(null, null, '/8');
+    his.pushState(null, null, "/8");
 }
 
 function checkForm() {
     var reqFormElems = doc.getElementsByClassName("requiredFormElement");
     var chckFormElem = doc.getElementsByClassName("checkFormElement")[0];
     var checkbox = chckFormElem.getElementsByTagName("input")[0];
-    var elem = 0;
+    var elem;
     var input;
     var inputName;
+    var inputValue;
     var regExp;
     serMsg.innerHTML = "";
-    for (elem; elem < reqFormElems.length; elem++) {
+    for (elem = 0; elem < reqFormElems.length; elem += 1) {
         input = reqFormElems[elem].getElementsByTagName("input")[0];
         inputName = input.getAttribute("name");
         inputValue = input.value;
@@ -64,7 +68,7 @@ function checkForm() {
                 return false;
             }
         } else if (inputName === "email") {
-            regExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]/;
+            regExp = /^[a-zA-Z0-9_.+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]/;
             if (!inputValue.match(regExp)) {
                 serMsg.innerHTML = "Поле: E-mail, заполнено некорректно";
                 return false;
@@ -80,37 +84,34 @@ function checkForm() {
 
 function submitForm() {
     const formData = {
-        fullName: doc.getElementsByName("fullName")[0].value,
-        email: doc.getElementsByName("email")[0].value,
-        phoneNumber: doc.getElementsByName("phoneNumber")[0].value,
-        organization: doc.getElementsByName("organization")[0].value,
-        message: doc.getElementsByName("message")[0].value,
         check: doc.getElementsByName("check")[0].checked,
+        email: doc.getElementsByName("email")[0].value,
+        fullName: doc.getElementsByName("fullName")[0].value,
+        message: doc.getElementsByName("message")[0].value,
+        organization: doc.getElementsByName("organization")[0].value,
+        phoneNumber: doc.getElementsByName("phoneNumber")[0].value
     };
-    var slapform = new Slapform();
+    var slapform = Slap;
     slapform.submit({
-        form: '6iAdlxl4o9',
         data: formData,
-    })
-    .then(function (response) {
-        console.log('Success', response);
-        alert("Форма была успешно отправлена на сервер");
+        form: "6iAdlxl4o9"
+    }).then(function (response) {
+        con.log("Success", response);
+        win.alert("Форма была успешно отправлена на сервер");
         clearForm();
-    })
-    .catch(function (e) {
-        console.error('Fail', e);
-        alert("При отправлении формы на сервер возникла ошибка");
-    })
+    }).catch(function (e) {
+        con.error("Fail", e);
+        win.alert("При отправлении формы на сервер возникла ошибка");
+    });
 }
 
 function clearForm() {
     saveValuesFromForm();
-    const popup = doc.getElementsByClassName("popup")[0];
     const popupElems = popup.getElementsByClassName("textElement");
     const popupCheckbox = popup.getElementsByClassName("checkFormElement")[0];
-    var elem = 0;
+    var elem;
     var elemInput;
-    for (elem; elem < popupElems.length; elem++) {
+    for (elem = 0; elem < popupElems.length; elem += 1) {
         if (elem !== 4) {
             elemInput = popupElems[elem].getElementsByTagName("input")[0];
         } else {
@@ -124,7 +125,8 @@ function clearForm() {
 function saveValuesFromForm() {
     const elements = popup.getElementsByClassName("textElement");
     var element;
-    for (let i = 0; i < elements.length; i++) {
+    var i;
+    for (i = 0; i < elements.length; i += 1) {
         if (i !== 4) {
             element = elements[i].getElementsByTagName("input")[0];
         } else {
@@ -137,13 +139,14 @@ function saveValuesFromForm() {
 function insertLastInformation() {
     const elements = popup.getElementsByClassName("textElement");
     var element;
-    for (let i = 0; i < elements.length; i++) {
+    var i;
+    for (i = 0; i < elements.length; i += 1) {
         if (i !== 4) {
             element = elements[i].getElementsByTagName("input")[0];
         } else {
             element = elements[i].getElementsByTagName("textarea")[0];
         }
-        element.value = localStorage.getItem(element.name); 
+        element.value = localStorage.getItem(element.name);
     }
     localStorage.clear();
 }
